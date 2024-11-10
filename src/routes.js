3b4c67c964bb5c54412dd67e9e79201d6d53e5ae
Gabriel Rebechi <bibo.rebechi@gmail.com>
2024-11-10 05:53:30 -0300

    feat: define a rota para deleteAnnotation via id na Api

--- src/routes.js
+++ src/routes.js
@@ -5,7 +5,7 @@ const routes = express.Router();
 
 routes.post("/annotations", AnnotationController.createAnnotation);
 routes.get("/annotations", AnnotationController.getAnnotations);
-//routes.put("/:id", updateAnnotation);
+routes.delete("/annotations/:id", AnnotationController.deleteAnnotation);
 //routes.delete("/:id", deleteAnnotation);
 
 export default routes;

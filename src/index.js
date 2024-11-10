1509b4019e75c207a106698f3782f8daf233e27e
Gabriel Rebechi <bibo.rebechi@gmail.com>
2024-11-09 22:37:06 -0300

    feat: configuração inicial do arquivo principal da aplicação que já se conecta ao banco de dados

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import * as dotenv from 'dotenv';
+import express from 'express';
+import routes from './routes.js';
+import { connectDB } from './config/dbConfig.js';
+dotenv.config();
+const app = express();
+
+
+app.use(express.json());
+app.use(routes);
+app.listen(3333, () => {connectDB()});

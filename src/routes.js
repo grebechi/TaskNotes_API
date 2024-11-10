66663041167441049d93aa762c03218439ffca34
Gabriel Rebechi <bibo.rebechi@gmail.com>
2024-11-10 07:15:01 -0300

    feat: adiciona a rota para updateContent

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,7 @@
 import express from "express";
 import * as AnnotationController from "./controllers/AnnotationController.js";
 import * as PriorityController from "./controllers/PriorityController.js";
+import * as ContentController from "./controllers/ContentController.js";
 
 const routes = express.Router();
 
@@ -13,6 +14,9 @@ routes.delete("/annotations/:id", AnnotationController.deleteAnnotation);
 // Rotas Priority
 routes.get("/priorities", PriorityController.getPriorityAnnotations);
 routes.post("/priorities/:id", PriorityController.changePriorityAnnotation);
+
+// Rotas Content
+routes.post("/contents/:id", ContentController.updateContentAnnotation);
 
 
 

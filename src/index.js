05783edaba708880bf5857c9c27e8f730a4864fe
Gabriel Rebechi <bibo.rebechi@gmail.com>
2024-11-10 07:24:10 -0300

    feat: utilização do cors para permitir acesso pelo front-end por portas diferentes

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import * as dotenv from 'dotenv';
 import express from 'express';
 import routes from './routes.js';
+import cors from 'cors';
 import { connectDB } from './config/dbConfig.js';
 dotenv.config();
 const app = express();
 
-
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 app.listen(process.env.SERVER_PORT, () => {connectDB()});
